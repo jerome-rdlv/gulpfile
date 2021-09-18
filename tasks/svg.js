365d09f3f917cb709373dc81a9e0c8845acb39e6
Jérôme Mulsant <jerome@rue-de-la-vieille.fr>
2021-09-18 18:14:19 +0200

    Nothing

--- tasks/svg.js
+++ tasks/svg.js
@@ -37,22 +37,22 @@ module.exports = function (config) {
             </script>
          */
         const plugins = [
-            {'name': 'removeDoctype'},
-            {'name': 'removeComments'},
-            {'name': 'removeTitle'},
-            {'name': 'convertStyleToAttrs'},
+            {name: 'removeDoctype'},
+            {name: 'removeComments'},
+            {name: 'removeTitle'},
+            {name: 'convertStyleToAttrs'},
             {
-                'name': 'cleanupIDs',
-                'params': {
+                name: 'cleanupIDs',
+                params: {
                     prefix: prefix + '-',
                     minify: true
                 }
             },
-            {'name': 'removeViewBox', active: false},
-            {'name': 'removeStyleElement'},
+            {name: 'removeViewBox', active: false},
+            {name: 'removeStyleElement'},
             {
-                'name': 'cleanupNumericValues',
-                'params': {
+                name: 'cleanupNumericValues',
+                params: {
                     floatPrecision: 5
                 }
             }
