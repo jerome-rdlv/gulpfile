e44c57bfb9d87b0c33108d992adb6b8608b1ec21
Jérôme Mulsant <jerome@rue-de-la-vieille.fr>
2021-10-05 20:22:33 +0200

    Upgrade babel-loader

---
+++ test/gulpfile.js
@@ -0,0 +1,26 @@
+/**
+ * @see node_modules/gulpfile/defaults.js for possible options.
+ * Some options can be overridden by CLI arguments:
+ *  - --url=http://example.org
+ *  - --production or --prod
+ */
+module.exports = require('../index.js')({
+    url: "https://example.org",
+    basePath: __dirname,
+    tasks: {
+        // static assets to copy
+//        copy: [
+//            'font/*.{woff,woff2}',
+//            '.htaccess',
+//            '.htaccess.production',
+//        ],
+        js: [
+            'js/main.js',
+        ],
+        scss: {
+            // because block editor can not transform compressed CSS
+            nonano: ['editor.css'],
+            engine: 'dart', // dart or node
+        }
+    }
+});
